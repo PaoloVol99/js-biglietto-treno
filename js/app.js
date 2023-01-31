8f3107b504f5ac7319f5c6bdcfb7b009df8d07a0
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-01-31 17:09:19 +0100

    FEAT+STYLE content, style and javascript added

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Chiedere numero km
+let kilometers = parseInt(prompt("Quanti km vuoi percorrere?"))
+console.log(kilometers)
+// Calcolare prezzo in base ai km
+let pricePerKm = 0.21
+let startingPrice = pricePerKm * kilometers
+console.log(startingPrice)
+// Chiedere età
+let age = parseInt(prompt("Quanti anni hai?"))
+console.log(age)
+// Calcolare sconto in base all'età
+let discountUnderage = startingPrice * 0.2
+let underagePrice = startingPrice - discountUnderage
+let discountOver = startingPrice * 0.4
+let overPrice = startingPrice - discountOver
+let noDiscountPrice = startingPrice.toFixed(2)
+let finalUnderagePrice = underagePrice.toFixed(2)
+let finalOverPrice = overPrice.toFixed(2)
+
+if (age < 18) {
+    console.log(underagePrice)
+    console.log(finalUnderagePrice)
+    document.getElementById("price").innerHTML = "€ " + finalUnderagePrice
+} else if (age > 64) {
+    console.log(overPrice)
+    console.log(finalOverPrice)
+    document.getElementById("price").innerHTML = "€ " + finalOverPrice
+} else {
+    console.log(noDiscountPrice)
+    document.getElementById("price").innerHTML = "€ " + noDiscountPrice
+}
+// Chiedere Nome e cognome
+let firstName = prompt("Come ti chiami?")
+let lastName = prompt("Qual è il tuo cognome?")
+console.log(firstName)
+console.log(lastName)
+// Stampare risultati
+document.getElementById("name").innerHTML = firstName + " " + lastName
